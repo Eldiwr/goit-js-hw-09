66bf4ad7d57854af8e15e8465238a7d5b3eaf10f
Eldiwr <Eldiwr97@gmail.com>
2022-07-06 12:00:20 +0300

    1st - done
    Автоматическая смена цвета body.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+
+let timerId = null;
+
+stopButton.setAttribute('disabled', 'true');
+
+startButton.addEventListener('click', randomBodyColor);
+stopButton.addEventListener('click', stopRandomBodyColor);
+
+function randomBodyColor() {
+    startButton.setAttribute('disabled', 'true');
+    stopButton.removeAttribute('disabled');
+
+    timerId = setInterval(() => {
+        bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopRandomBodyColor() {
+    clearInterval(timerId);
+
+    startButton.removeAttribute('disabled');
+    stopButton.setAttribute('disabled', 'true');
+}
